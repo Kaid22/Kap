876c9960d26e9b3804bd4824f18d17de677c4236
Kaid22 <avdickaid@gmail.com>
2021-04-08 14:02:54 +0200

    komita

--- kap/Form1.cs
+++ kap/Form1.cs
@@ -16,5 +16,39 @@ namespace kap
         {
             InitializeComponent();
         }
+        Kap kap = new Kap();
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kap.Postavi(ClientRectangle.Width / 2, 100, 20);
+            
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            kap.Pomeri(5);
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            kap.Crtaj(e.Graphics);
+            //Flasa
+            Graphics g = CreateGraphics();
+            int a = ClientRectangle.Height;
+            int b = ClientRectangle.Width;
+            Pen pen = new Pen(Color.Black, 3);
+            Brush brush = new SolidBrush(Color.Black);
+            g.DrawRectangle(pen,b/2 - 50, a - 100, 100, 100);
+            g.FillRectangle(brush, b / 2 - 50, a - 100, 100, 100);
+            g.DrawRectangle(pen, b / 2 - 20, a - 150, 40, 150);
+            g.FillRectangle(brush, b / 2 - 20, a - 150, 40, 150);
+        }
     }
 }
---
+++ kap/Kap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace kap
+{
+    class Kap
+    {
+        
+        
+            int x, y, a;
+            public void Postavi(int x, int y, int a)
+            {
+                this.x = x;
+                this.y = y;
+                this.a = a;
+            }
+            public void Crtaj(Graphics g)
+            {
+                
+                Pen pen = new Pen(Color.Blue, 3);
+                SolidBrush brush = new SolidBrush(Color.Blue);
+                Point A = new Point(x, y - 2 * a);
+                Point B = new Point(x - a, y);
+                Point C = new Point(x + a, y);
+
+                g.DrawLine(pen,A,B);
+                g.DrawLine(pen, A,C);
+                PointF[] pointF = { A, B, C };
+                g.FillPolygon(brush,pointF );
+                g.DrawArc(pen, x-a, y-a, a*2, a*2, 0, 180);
+                g.FillPie(brush, x - a, y - a, a * 2, a * 2, 0, 180);
+
+            }
+            public void Pomeri(int dy)
+            {
+                y += dy;
+            }
+        }
+    }
+
